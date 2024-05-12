e2cf16cf0dfdef2a4fe8e922f5109b842ccb1e93
ElMaikina <mikesoto890@gmail.com>
2024-05-11 22:42:34 -0400

    crear el controlador

--- tarea-2/Estado.java
+++ tarea-2/Estado.java
@@ -7,32 +7,3 @@ public enum Estado {
     CANSADO,     
     MUERTO                       
 }
-
-
-// public void printEstado(Estado estado){
-//     switch(estado){
-//         case NEUTRO:
-//             System.out.println("(-_-) Meh....");
-//             break;
-//         case FELIZ:
-//             System.out.println("(^_^)/ Weeeeeh!");
-//             break;
-//         case TRISTE:
-//             System.out.println("(._.) snif....");
-//             break;
-//         case HAMBRIENTO:
-//             System.out.println("(0o0) hambre hambre! ");
-//             break;
-//         case ENOJADO:
-//             System.out.println("(ôwô) grrrr....");
-//             break;
-//         case CANSADO:
-//             System.out.println("(=_=) zzzz....");
-//             break;
-//         case MUERTO:
-//             System.out.println("(x_x) fin del juego");
-//             break;
-//     }
-// }
-
-
