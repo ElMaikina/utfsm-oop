c531d3254dbe5ca9b88fc36583be3aac6f06e45d
Sebastian.Moya <sebastian.moya@usm.cl>
2024-05-17 16:16:27 -0400

    Cambios a la clase
    clase controller (estoy haciendo nuevos cambios para ver si puedo dejarlo listo de una

---
+++ tarea-1/Controller.java
@@ -0,0 +1,10 @@
+import javafx.util.Duration;
+import javafx.animation.Timeline;
+import javafx.animation.KeyFrame;
+
+public class Controller {
+    private Mascota mi_mascota;
+    private Inventario inventario;
+    private boolean estado_luz = true;
+    private Timeline timeline;
+}
