4a8f90621dc8e7586698b4865fce07166addcab2
Sebastian.Moya <127323694+UsmMoya@users.noreply.github.com>
2024-05-21 19:58:48 -0400

    Add files via upload
    Controller actualizado, ahora deberia funcionar el timer, ademas de que meti la funcion actionluz que servira para encender y apagar la luz

---
+++ tarea-2/Controller.java
@@ -0,0 +1,38 @@
+import javafx.animation.*;
+import javafx.animation.Timeline;
+import javafx.animation.KeyFrame;
+import javafx.util.*;
+import javafx.util.Duration;
+
+public class Controller 
+{
+    private Mascota mi_mascota;
+    private Inventario inventario;
+    private boolean estado_luz = true;
+    private Timeline timeline;
+    private KeyFrame keyframe;
+    public Controller (Mascota mascota){
+        this.mi_mascota=mascota;
+    
+    }
+ 
+
+    public void start_timer(){
+        keyframe = new KeyFrame(Duration.seconds(0.5),event -> {
+            mi_mascota.pasarTiempo();
+        });
+        timeline = new Timeline(keyframe);
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.play();
+        
+
+    };
+    public void actionLuz(){
+        if (estado_luz=true){
+            estado_luz=false;
+        }
+        else{
+            estado_luz=true;
+        }
+    }}
+ 
