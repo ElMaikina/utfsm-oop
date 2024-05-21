2c60cb5ea8a129bb2e85211cfb933cf0e71d5f0a
Sebastian.Moya <sebastian.moya@usm.cl>
2024-05-21 18:00:31 -0400

    Update Controller.java

--- tarea-1/Controller.java
+++ tarea-1/Controller.java
@@ -2,9 +2,15 @@ import javafx.util.Duration;
 import javafx.animation.Timeline;
 import javafx.animation.KeyFrame;
 
-public class Controller {
+public class Controller 
+{
     private Mascota mi_mascota;
     private Inventario inventario;
     private boolean estado_luz = true;
     private Timeline timeline;
+
+    public void start_timer(){
+
+    };
+    public void update_status (){};
 }
