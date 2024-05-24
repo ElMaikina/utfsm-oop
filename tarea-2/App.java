a2484ec53cc1332ed91692a1141a6fc344bc266a
ElMaikina <mikesoto890@gmail.com>
2024-05-24 13:26:06 -0400

    quitar comentarios de la interfaz

--- tarea-2/App.java
+++ tarea-2/App.java
@@ -12,25 +12,6 @@ public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        /*
-        // Create a button
-        Button btn = new Button();
-        btn.setText("Click me!");
-        // Set action when button is clicked
-        btn.setOnAction(e -> showMessage());
-
-        // Create a layout and add the button to it
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-
-        // Create the scene and add the layout to it
-        Scene scene = new Scene(root, 300, 250);
-
-        // Set the scene to the stage and show the stage
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("Basic JavaFX App");
-        primaryStage.show();
-        */
         Parent root = FXMLLoader.load(getClass().getResource("VistaPrincipal.fxml"));
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
