854a682fe7eff3e2104236783cbf225a12bf6c4e
ElMaikina <mikesoto890@gmail.com>
2024-06-18 01:46:19 -0400

    calzar los metodos de item con los originales

--- tarea-3/etapa-1/Item.cpp
+++ tarea-3/etapa-1/Item.cpp
@@ -4,23 +4,36 @@
 // Definición de la clase Item
 class Item {
 private:
+    int id;
+    std::string tipo;
     std::string nombre;
     int cantidad;
 
 public:
     // Constructor
-    Item(std::string nombre, int cantidad) : nombre(nombre), cantidad(cantidad) {}
-
+    Item(int i, std::string t, std::string n, int c) {
+        id = i;
+        tipo = t;
+        nombre = n;
+        cantidad = c;
+    }
     // Métodos para acceder a los atributos
-    std::string getNombre() const {
+    int getId() {
+        return id;
+    }
+    std::string getTipo() {
+        return tipo;
+    }
+    std::string getNombre() {
         return nombre;
     }
-
-    int getCantidad() const {
+    int getCant() const {
         return cantidad;
     }
-
-    void setCantidad(int nuevaCantidad) {
-        cantidad = nuevaCantidad;
+    void setCant(int c) {
+        cantidad = c;
+    }
+    void mostrarItem() {
+        std::cout << id << ": " << nombre << ", cantidad " << cantidad <<  std::endl;
     }
 };
