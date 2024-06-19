ebc7bde36cc810a8cafc70550d73eac133799f21
joaquinjarah <167722594+joaquinjarah@users.noreply.github.com>
2024-06-18 23:22:29 -0400

    Update Inventario.h
    Prototipo báscio de Inventario.h
    Falta incluir clases Item y Mascota.
    Falta calibrar bien tipos de variable que retornan las funciones.

--- tarea-3/etapa-2/headers/Inventario.h
+++ tarea-3/etapa-2/headers/Inventario.h
@@ -1,1 +1,26 @@
+//#include "Item.h"
+//#include "Macota.h"
+#include <vector>
 
+class Mascota;
+class Item;
+
+class Inventario{
+    
+    public:
+    Inventario(Mascota &mascota);
+    Inventario(Inventario &original);
+    ~Inventario();
+
+    std::vector<Item> abrirInventario()const;
+
+    void agregarItem(Item item);
+    void consumirItem(Item item);
+    Item buscarItem(int ID);
+
+    private:
+    Mascota *mascota;
+    std::vector<Item> invJuguetes;
+    std::vector<Item> invComida;
+    std::vector<Item> invMedicina;
+};
