2fb8e979c05cd924ddd88d691591ef169d7c9c24
ElMaikina <mikesoto890@gmail.com>
2024-06-21 15:34:29 -0400

    conflictos entre implementaciones arreglados

--- tarea-3/etapa-1/Main.cpp
+++ tarea-3/etapa-1/Main.cpp
@@ -10,21 +10,16 @@ int main(int argc, char **argv) {
     // Crear y mostrar estado inicial de la mascota
     Mascota miMascota("Garfield");
 	miMascota.mostrarMascota();
-    //miMascota.mostrarEstado();
-
-   	//// Crear instancias de ítems
-    //Comida queso(1, "Queso", 5);
-    //Medicina jarabe(2, "Jarabe", 3);
 
     // Crear instancias de ítems
     Comida queso(1, "Queso", 5);
     Medicina jarabe(2, "Jarabe", 3);
     Juguete pelota(3, "Pelota");  // Juguete ilimitado
 
-    //// Mostrar información de ítems
-    //queso.mostrarItem();
-    //jarabe.mostrarItem();
-    //pelota.mostrarItem();
+    // Mostrar información de ítems
+    queso.mostrarItem();
+    jarabe.mostrarItem();
+    pelota.mostrarItem();
 
     //// Usar ítems en la mascota y mostrar su estado después de cada uso
     //queso.usar(miMascota);
