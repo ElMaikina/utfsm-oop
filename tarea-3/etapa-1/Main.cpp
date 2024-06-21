ad0ad62d26a278be36c70c85742126e3d8b817bb
joaquinjarah <167722594+joaquinjarah@users.noreply.github.com>
2024-06-21 02:37:13 -0400

    Se agregan los headers a Main

--- tarea-3/etapa-1/Main.cpp
+++ tarea-3/etapa-1/Main.cpp
@@ -1,16 +1,19 @@
-// Archivo base para pruebas de etapa 3.1
+#include <iostream>
+#include "../headers/Mascota.h"
+#include "../headers/Juguete.h"
+#include "../headers/Comida.h"
+#include "../headers/Medicina.h"
 
-enum Estado { Neutro, Feliz, Triste, Hambriento, Enojado, Cansado, Muerto };
 
-int main() {
+int main(int argc, char **argv) {
     // Crear y mostrar estado inicial de la mascota
     Mascota miMascota("Garfield");
-    miMascota.mostrarEstado();
+    miMascota.printEstado();
 
     // Crear instancias de ítems
     Comida queso(1, "Queso", 5);
     Medicina jarabe(2, "Jarabe", 3);
-    Juguete pelota(3, "Pelota", -1);  // Juguete ilimitado
+    Juguete pelota(3, "Pelota");  // Juguete ilimitado
 
     // Mostrar información de ítems
     queso.mostrarItem();
