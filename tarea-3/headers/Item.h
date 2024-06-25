a056a49e22879b5ac34243d4b0577164e331e787
joaquinjarah <167722594+joaquinjarah@users.noreply.github.com>
2024-06-25 18:04:13 -0400

    Ahora clase Item es abstracta
    - Función usarItem/usar ahora es virtual pura.
    - Se define destructor como virtual y default para correcto uso de referencias de clase base sobre instancias de subclase.
    - Variable "cantidad" ahora tiene accesibilidad protected

--- tarea-3/headers/Item.h
+++ tarea-3/headers/Item.h
@@ -6,70 +6,117 @@
 // Declaración incompleta de Mascota.
 class Mascota;
 
-/* 
- * Item es la clase abstracta padre las subclases: Juguete, Comida y Medicina.
+/**
+ * @class Item
+ * 
+ * @brief Clase abstracta padre las subclases: Juguete, Comida y Medicina.
+ *
  * Esta clase posee todos los métodos disponibles para cada subclase de Item.
  * Lógica tras la ejecución de funciones miembro (excepto SETers y GETers) varían según sub-clase.
  */
 class Item {
 
 private:
+    /** Número identificador del Item. */
     int id;
+
+    /** Tipo de Item, define sub-clase. */     
     std::string tipo;
+
+    /** Nombre de Item. */
     std::string nombre;
+
+protected:
+    /** Cantidad de Item. */
     int cantidad;
     
 public:
-    /*
-     * Método constructor.
+    /**
+     * @brief Método constructor de Item.
      *
      * @param id        Número identificador del Item.
      * 
-     * @param tipo      Tipo de Item, definidor de sub-clase.
+     * @param tipo      Tipo de Item, define de sub-clase.
      * 
      * @param nombre    Nombre de Item.
      * 
      * @param cantidad  Cantidad de Item.
-     * Si cantidad no se especifica, cantidad = -1.
+     * Default = -1.
      */
-    Item(int id, std::string tipo, std::string nombre, int cantidad);
+    Item(int id, std::string tipo, std::string nombre, int cantidad=-1);
 
-    /*
-     * Consume Item en Mascota, afectando sus estadísticas.
+    /**
+     * @brief Método destructor de Item.
+     *
+     * El destructor es virtual para que instancias de subclases de item que se
+     * encuentren bajo referencias o punteros de clase Item puedan ser destruidos
+     * correctamente.
+     */
+    virtual ~Item() = default;
+
+    /**
+     * @brief Consume Item en Mascota, afectando sus estadísticas.
+     *
      * Al consumir: "cantidad" -= 1.
      *
      * @param mascota
      * Mascota que consume el Item.
      */
-    virtual void usarItem(Mascota &mascota);
+    virtual void usar(Mascota &mascota) = 0;
 
-    // Muestra Item en consola.
+    /**
+     * @brief Muestra representación de Item en consola.
+     *
+     * Muestra salida en consola/terminal listado de atributos
+     * de instancia de subclase de Item.
+     */
     void mostrarItem() const;
 
-    // @returns id
+    /**
+     * @brief Retorna valor entero Id asociado a Item.
+     *
+     * Id es un identificador numérico para cada Item.
+     * 
+     * @return Id de Item.
+     */
     int getId() const; 
 
-    // @returns nombre
-    std::string getNombre();
-
-    // @returns tipo
-    std::string getTipo();
-
-    // @returns cantidad
-    int getCant();
-
-    /*
-     * Cambia el valor de miebro dato "cantidad"
+    /**
+     * @brief Retorna string Nombre asociado a Item.
+     *
+     * Nombre es el string que identifica a cada Item.
      * 
-     * @param  nuevaCantidad
-     * Número entero que reemplazará valor de "cantidad".
+     * @return Nombre de Item.
      */
-    void setCant(int nuevaCantidad);
+    std::string getNombre() const;
 
-	// Se definen las tres subclases que heredan de Item
-	friend class Comida;
-	friend class Medicina;
-	friend class Juguete;
+    /**
+     * @brief Retorna string Tipo asociado a Item.
+     * 
+     * Tipo es el string que identifica a qué subclase de Item pertenece la
+     * instancia derivada de Item (Juguete, Comida o Medicina).
+     * 
+     * @return Tipo (subclase) de Item.
+     */
+    std::string getTipo() const;
+
+    /**
+     * @brief Retorna valor entero Cantidad asociado a Item.
+     * 
+     * Cantidad representa la cantidad de Items disponibles por instancia.
+     * 
+     * @return Cantidad disponible de Item.
+     */
+    int getCantidad() const;
+
+    /**
+     * @brief Cambia el valor de Cantidad asociada a Item.
+     * 
+     * Cantidad representa la cantidad de Items disponibles por instancia.
+     * 
+     * @param nuevaCantidad Número entero que reemplazará valor de Cantidad.
+     */
+    void setCantidad(int nuevaCantidad);
 };
 
 #endif // ITEM_H
