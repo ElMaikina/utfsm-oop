5dac3b16d0513959faa32509913244ceec8f4d23
joaquinjarah <167722594+joaquinjarah@users.noreply.github.com>
2024-06-25 18:34:32 -0400

    Ahora constructor funciona con lista de inicialización
    Se elimina definición de usar porque es una función virtual pura para la clase Item

--- tarea-3/etapa-2/Item.cpp
+++ tarea-3/etapa-2/Item.cpp
@@ -1,33 +1,42 @@
 #include <iostream>
-#include <string>
 #include "../headers/Item.h"
-#include "../headers/Mascota.h"
 
-Item::Item(int i, std::string t, std::string n, int c) {
-	id = i;
-	tipo = t;
-	nombre = n;
-	cantidad = c;
-}
-int Item::getId() const {
-	return id;
-}
-void Item::usarItem(Mascota &mascota) {
-	std::cout << "Usando " << tipo << " " << nombre << " en la Mascota" << std::endl;
-	mascota.mostrarMascota();
-} 
-std::string Item::getTipo() {
-	return tipo;
-}
-std::string Item::getNombre() {
-	return nombre;
-}
-int Item::getCant() {
-	return cantidad;
-}
-void Item::setCant(int newCant) {
-	cantidad = newCant;
-}
+using std::string;
+using std::cout;
+using std::endl;
+
+Item::Item(int id, string tipo, string nombre, int cantidad) :
+    id(id), 
+    tipo(tipo), 
+    nombre(nombre), 
+    cantidad(cantidad)
+    {}
+
 void Item::mostrarItem() const {
-	std::cout << "ID: " << id << ", Nombre: " << nombre << ", Cantidad " << cantidad << std::endl;
-};
+    cout 
+    << "ID: "         << id
+    << "\nNombre: "   << nombre
+    << "\nCantidad: " << cantidad
+    << "\n" 
+    << endl;
+}
+
+int Item::getId() const {
+    return id;
+}
+
+string Item::getTipo() const {
+    return tipo;
+}
+
+string Item::getNombre() const {
+    return nombre;
+}
+
+int Item::getCantidad() const {
+    return cantidad;
+}
+
+void Item::setCantidad(int nuevaCantidad) {
+    this->cantidad = nuevaCantidad;
+}
