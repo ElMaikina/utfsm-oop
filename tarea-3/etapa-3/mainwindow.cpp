1d766acba9838ad0f1c7eadca723f70ec3a167bf
joaquinjarah <167722594+joaquinjarah@users.noreply.github.com>
2024-06-28 05:05:10 -0400

    Se cambia imagen de goldie por otro jpg
    El problema de la imagen invisible era el formato png, se necesita un pluguin para que Qt soporte formato png

--- tarea-3/etapa-3/mainwindow.cpp
+++ tarea-3/etapa-3/mainwindow.cpp
@@ -1,24 +1,32 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
-#include <QPixmap>
+
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    QPixmap pix (":/resource/img/goldie.png");
+
+    this->setFixedSize(600,410);
+    this->setWindowTitle("Perrito");
+
+    QPixmap pix(":/resource/img/dog.jpg");
     QPixmap j1(":/resource/img/bone.jpg");
     QPixmap j2(":/resource/img/ball.jpg");
+
     QIcon HuesoBoton(j1);
     QIcon BolaBoton(j2);
+
     ui->Hueso->setIcon(HuesoBoton);
     ui->Bola->setIcon(BolaBoton);
+
     ui->Hueso->setIconSize(QSize(50,50));
     ui->Bola->setIconSize(QSize(50,50));
-    int w=ui->Mascota->width();
-    int h=ui->Mascota->height();
-    ui->Mascota->setPixmap(pix.scaled(h,w,Qt::KeepAspectRatio));
+
+    int w = ui->Mascota->width();
+    int h = ui->Mascota->height();
+    ui->Mascota->setPixmap(pix.scaled(w,h));
 }
 
 MainWindow::~MainWindow()
