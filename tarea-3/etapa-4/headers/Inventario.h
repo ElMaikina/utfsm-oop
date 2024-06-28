a75c7f2aa4c23013204fd15edd9bb7541525362e
Sebastian.Moya <127323694+UsmMoya@users.noreply.github.com>
2024-06-28 14:34:52 -0400

    Comentarios para  doxygen para Inventario.h

--- tarea-3/etapa-4/headers/Inventario.h
+++ tarea-3/etapa-4/headers/Inventario.h
@@ -5,7 +5,8 @@
 #include "Item.h"
 #include "Mascota.h" 
 
-// Definición de la clase Inventario
+/* @class Inventario 
+@brief Definición de la clase Inventario */
 class Inventario {
 private:
 	std::vector<Item*> items;
@@ -13,22 +14,31 @@ private:
 
 public:
     // Constructor
-    Inventario(Mascota &mascota);
+    Inventario(Mascota &mascota);/*@brief Constructor de inventario 
+    @param &mascota	puntero a la mascota
+    */
 
     // Destructor
-    ~Inventario();
+    ~Inventario();/*@brief destructor */
 
     // Método para agregar un nuevo item al inventario
-    void agregarItem(Item *item);
+    void agregarItem(Item *item);/* @brief Agrega un item al inventario 
+    @param *item	Operador punterio de item	
+    */
 
     // Método para buscar Item por ID
-    Item * buscarPorID(int id);
+    Item * buscarPorID(int id);/* @brief busca el item por id numerico 
+    @param id	id numerico del item
+    */
 
     // Método para usar un nuevo item del inventario
-    void usarItem(int id, Mascota &mascota);
+    void usarItem(int id, Mascota &mascota); /* @brief utiliza un Item del inventario 
+@param id	id numerico del item
+@param &mascota	referencia a la mascota
+*/
 
     // Método para imprimir todos los items del inventario
-    void mostrarInventario() const;
+    void mostrarInventario() const;/*@brief imprime todos los item del inventario*/
 };
 
 #endif // INVENTARIO_H
