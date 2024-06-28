d5c4befa2ed9629250773cfec6c33abb600c412d
Sebastian.Moya <127323694+UsmMoya@users.noreply.github.com>
2024-06-28 14:26:47 -0400

    Comentarios doxygen para Comida.h actualizado

--- tarea-3/etapa-4/headers/Comida.h
+++ tarea-3/etapa-4/headers/Comida.h
@@ -3,13 +3,19 @@
 #include "Item.h"
 
 class Comida : public Item { 
+/** @Class Comida */
 
 public:
-    Comida(int id, std::string nombre, int cantidad);/**Constructor Comida */
+    Comida(int id, std::string nombre, int cantidad);/**@brief Constructor Comida
+    @param id    id numerico del item
+    @param nombre    nombre de la comida
+    @param cantidad    cantidad total del item
+    */
 
-    ~Comida() override = default;/**Destructor Comida */
+    ~Comida() override = default;/**@brief Destructor Comida */
 
-    void usar(Mascota &mascota) override;/** Funcion para utilizar alimentar a la mascota*/
+    void usar(Mascota &mascota) override;/**@brief  Funcion para utilizar alimentar a la mascota
+    @param &mascota Puntero de mascota*/
 };
 
 #endif //COMIDA_H
