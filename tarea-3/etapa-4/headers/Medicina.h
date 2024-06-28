7318f779a2d25bbb5b9d38ecbe9ba212a10c8c0d
Sebastian.Moya <127323694+UsmMoya@users.noreply.github.com>
2024-06-28 14:18:01 -0400

    Comentarios para doxygen con los @ para medicina.h

--- tarea-3/etapa-4/headers/Medicina.h
+++ tarea-3/etapa-4/headers/Medicina.h
@@ -1,16 +1,23 @@
 #ifndef MEDICINA_H
 #define MEDICINA_H
 #include "Item.h"
-/** Sub clase de Item hereda los elementos publicos de la misma */
+/** @Class Medicina
+
+
+@brief Sub clase de Item hereda los elementos publicos de la misma */
 
 class Medicina : public Item { 
 
 public:
-    Medicina(int id, std::string nombre, int cantidad);/** Constructor para la clase medicina*/
+    Medicina(int id, std::string nombre, int cantidad);/** @brief Constructor para la clase medicina
+    @param id    Numero identificador de item
+    @param nombre    Nombre de la medicina
+    @param cantidad    Cantidad total de la medicina especifica en el inventario*/
 
-    ~Medicina() override = default;/** Destructor de Medicina*/
+    ~Medicina() override = default;/**@brief Destructor de Medicina*/
 
-    void usar(Mascota &mascota) override; /** Funcion para utilizar la medicina y aumentar sus puntos de salud*/
+    void usar(Mascota &mascota) override; /** @brief Funcion para utilizar la medicina y aumentar sus puntos de salud
+    @param &mascota    puntero apuntando a las caracteristicas de la mascota  */
 };
 
 #endif //MEDICINA_H
