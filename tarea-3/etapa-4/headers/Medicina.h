e456b630ceb06b41be7f9c4911be0da21abc6957
Sebastian.Moya <127323694+UsmMoya@users.noreply.github.com>
2024-06-28 14:02:04 -0400

    Comentarios para doxygen

--- tarea-3/etapa-4/headers/Medicina.h
+++ tarea-3/etapa-4/headers/Medicina.h
@@ -1,15 +1,16 @@
 #ifndef MEDICINA_H
 #define MEDICINA_H
 #include "Item.h"
+/** Sub clase de Item hereda los elementos publicos de la misma */
 
 class Medicina : public Item { 
 
 public:
-    Medicina(int id, std::string nombre, int cantidad);
+    Medicina(int id, std::string nombre, int cantidad);/** Constructor para la clase medicina*/
 
-    ~Medicina() override = default;
+    ~Medicina() override = default;/** Destructor de Medicina*/
 
-    void usar(Mascota &mascota) override;
+    void usar(Mascota &mascota) override; /** Funcion para utilizar la medicina y aumentar sus puntos de salud*/
 };
 
 #endif //MEDICINA_H
