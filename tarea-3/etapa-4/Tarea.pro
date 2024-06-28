41d18cab7f0e4d9b59d891b30636bb8303c14c0c
joaquinjarah <167722594+joaquinjarah@users.noreply.github.com>
2024-06-28 14:36:39 -0400

    Se suben archivos de Interfaz gráfica etapa3

---
+++ tarea-3/etapa-4/Tarea.pro
@@ -0,0 +1,35 @@
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+CONFIG += c++17
+
+# You can make your code fail to compile if it uses deprecated APIs.
+# In order to do so, uncomment the following line.
+#DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
+
+SOURCES += main.cpp \
+    mainwindow.cpp \
+    game_src/Inventario.cpp \
+    game_src/Mascota.cpp \
+    game_src/Item.cpp \
+    game_src/Items.cpp
+
+
+HEADERS += mainwindow.h \
+    game_headers/Inventario.h \
+    game_headers/Mascota.h \
+    game_headers/Item.h \
+    game_headers/Juguete.h \
+    game_headers/Comida.h \
+    game_headers/Medicina.h
+
+
+FORMS += mainwindow.ui
+
+# Default rules for deployment.
+qnx: target.path = /tmp/$${TARGET}/bin
+else: unix:!android: target.path = /opt/$${TARGET}/bin
+!isEmpty(target.path): INSTALLS += target
+
+RESOURCES += recursos.qrc
---
+++ tarea-3/etapa-4/mainwindow.cpp
@@ -0,0 +1,49 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    this->setFixedSize(600,410);
+    this->setWindowTitle("Perrito");
+
+    QPixmap pix(":/resource/img/dog.jpg");
+    QPixmap j1(":/resource/img/bone.jpg");
+    QPixmap j2(":/resource/img/ball.jpg");
+
+    QIcon HuesoBoton(j1);
+    QIcon BolaBoton(j2);
+
+    ui->Hueso->setIcon(HuesoBoton);
+    ui->Bola->setIcon(BolaBoton);
+
+    ui->Hueso->setIconSize(QSize(50,50));
+    ui->Bola->setIconSize(QSize(50,50));
+
+    int w = ui->Mascota->width();
+    int h = ui->Mascota->height();
+    ui->Mascota->setPixmap(pix.scaled(w,h));
+
+
+
+    reloj = new QTimer(this);
+    connect(reloj, &QTimer::timeout, this, &MainWindow::advance);
+    reloj->start(500);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::advance()
+{
+
+
+}
+
