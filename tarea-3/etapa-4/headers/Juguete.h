fe79d3aaf5caa1be762302176f02cf418f3cd63e
Sebastian.Moya <127323694+UsmMoya@users.noreply.github.com>
2024-06-28 14:23:31 -0400

    Comentarios para doxygen en Juguete.h actualizados

--- tarea-3/etapa-4/headers/Juguete.h
+++ tarea-3/etapa-4/headers/Juguete.h
@@ -1,15 +1,23 @@
 #ifndef JUGUETE_H
 #define JUGUETE_H
 #include "Item.h"
-
+/** @Class Juguete
+@brief sub clase de item, jugute
+*/
 class Juguete : public Item { 
 
 public:
-    Juguete(int id, std::string nombre);/** Constructor de juguete */
+    Juguete(int id, std::string nombre);/** @brief Constructor de juguete 
+    @param id    numero identificador del item
+    @param nombre    nombre del juguete
+    */
 
-    ~Juguete() override = default;/** Destructor de juguete */
+    ~Juguete() override = default;/** @brief Destructor de juguete */
 
-    void usar(Mascota &mascota) override; /** Funcion para usar juguete*/
+    void usar(Mascota &mascota) override; /** @brief Funcion para usar juguete
+    @param &mascota    direccion para los datos de mascota apuntada
+    
+    */
 };
 
 #endif //JUGUETE_H
